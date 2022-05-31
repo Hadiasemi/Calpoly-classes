0cb2bed6e995d02ee1c66ff677e0547854674c97
Hadi Asemi <hadiasemi20@gmail.com>
2022-05-31 00:50:36 -0700

    added main.go and updated version of README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Calpoly-classes
+
+Get the classes, name, and number of units in you terminal. This code is written in go language.
+
+```go
+go get .
+go build -o class -ldflags="-s -w" main.go
+```
+
+## Usage:
+Please choose between csc, cpe, data, stat, math to go printed table of your classes.
+
+```bash
+usage: ./class [csc | cpe | data | stat | math]
+```
+
+![Sample Outp](./1.png)
+
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+        "fmt"
+	"strings"
+        "os"
+
+        "github.com/jedib0t/go-pretty/v6/table"
+        "github.com/gocolly/colly/v2"
+)
+
+func usage() {
+    fmt.Fprintf(os.Stderr, "usage: %s [csc | cpe | data | stat | math]\n", os.Args[0])
+    os.Exit(2)
+}
+
+func main() {
+    if (len(os.Args) != 2){
+        usage()
+    }
+
+    c := colly.NewCollector()
+    t := table.NewWriter()
+    t.SetOutputMirror(os.Stdout)
+    t.AppendHeader(table.Row{"Class", "Name", "Unit"})
+
+    c.OnHTML(".courseblock strong", func(e *colly.HTMLElement) {
+                
+		info := e.Text
+                s := strings.Replace(info, "\n", "", -1)
+                s1 := strings.Split(s, ".")
+                t.AppendRow([]interface{}{s1[0], s1[1], s1[2]})
+                t.AppendSeparator()
+
+	})
+
+    c.Visit("https://catalog.calpoly.edu/coursesaz/" + string(os.Args[1]) + "/")
+    t.Render()
+}
+
